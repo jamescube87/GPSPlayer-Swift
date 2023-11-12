c5bcdcdc7c42e6fd6df0126bc7e375288ae72551
James Jung <cube@jellybus.com>
2023-11-13 00:09:25 +0900

    GPSPlayer SwiftPackage and GPSPlayerView Init

---
+++ GPSPlayerSample/GPSPlayerSample/GPSPlayerSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPSPlayerSampleApp.swift
+//  GPSPlayerSample
+//
+//  Created by James Jung on 11/12/23
+//
+
+import SwiftUI
+
+@main
+struct GPSPlayerSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GPSPlayerView/Sources/GPSPlayerView/GPSPlayerView.swift
@@ -0,0 +1,12 @@
+
+import UIKit
+
+class GPSPlayerView : UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError()
+    }
+}
